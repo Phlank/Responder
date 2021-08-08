d3d619b74ac1d914226a50a1dadae850faebad5f
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2021-08-08 12:27:43 -0400

    #39 Fix all doc warnings, #42 Create project logo

--- src/ApiModeling/Extensions/ApiModelingServiceCollectionExtensions.cs
+++ src/ApiModeling/Extensions/ApiModelingServiceCollectionExtensions.cs
@@ -3,10 +3,31 @@ using System;
 
 namespace Phlank.ApiModeling.Extensions
 {
+    /// <summary>
+    /// Extensions to the <see cref="IServiceCollection"/> interface for
+    /// configuring the <see cref="IApiResultBuilder"/> as a part of the service
+    /// collection.
+    /// <para>
+    /// Simple usage:
+    /// </para>
+    /// <code>
+    /// public void ConfigureServices(IServiceCollection services)
+    /// {
+    ///     ...
+    ///     services.ConfigureApiResponseBuilder();
+    ///     ...
+    /// }
+    /// </code>
+    /// <para>
+    /// This will add the IApiResponseBuilder as a transient service to the
+    /// service collection.
+    /// </para>
+    /// </summary>
     public static class ApiModelingServiceCollectionExtensions
     {
         /// <summary>
-        /// Adds ApiResponse services to the specified <see cref="IServiceCollection" />.
+        /// Adds ApiResponse services to the specified
+        /// <see cref="IServiceCollection"/>.
         /// </summary>
         public static void ConfigureApiResponseBuilder(this IServiceCollection services, Action<ApiModelingOptionsBuilder> configureOptions = null)
         {
