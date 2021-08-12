b0afd3c96f1e0859618aa71a9a91e66b96bbbaad
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2021-08-11 23:31:35 -0400

    #38 Add integration test for model state response factory

---
+++ src/Tests/Helpers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phlank.ApiModeling.Tests.Helpers
+{
+    [ApiController]
+    public class TestController
+    {
+        private readonly IApiResultBuilder _resultBuilder;
+
+        public TestController(IApiResultBuilder resultBuilder) 
+        {
+            _resultBuilder = resultBuilder;
+        }
+
+        [Route("api/TestMethod")]
+        public ApiResult TestMethod(TestModel model)
+        {
+            return _resultBuilder.Build();
+        }
+    }
+}
---
+++ src/Tests/Helpers/TestStartup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Phlank.ApiModeling.Extensions;
+
+namespace Phlank.ApiModeling.Tests.Helpers
+{
+    public class TestStartup
+    {
+        public TestStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.ConfigureApiResponseBuilder(options =>
+            {
+                options.UseApiResponseForModelStateErrors();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
