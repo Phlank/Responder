fa8cc39ae70973fddf5352c82a61dc72eeb6010c
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2021-08-15 16:31:41 -0400

    #57 Add methods

---
+++ src/Responder/Extensions/ProblemDetailsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Phlank.Responder.Extensions
+{
+    internal static class ProblemDetailsExtensions
+    {
+        public static ApiError ToApiError(this ProblemDetails problemDetails)
+        {
+            return new ApiError
+            {
+                Detail = problemDetails.Detail,
+                Extensions = new Dictionary<string, object>(problemDetails.Extensions),
+                Instance = new Uri(problemDetails.Instance),
+                Status = (HttpStatusCode)problemDetails.Status.Value,
+                Title = problemDetails.Title,
+                Type = new Uri(problemDetails.Type)
+            };
+        }
+    }
+}
