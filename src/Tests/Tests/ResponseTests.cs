a733f3c1a9eb49885d9e94038be8f1f71da2c4b8
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2021-10-15 11:10:11 -0400

    #64: Added tests for Response

---
+++ src/Tests/Tests/ResponseTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phlank.Responder.Tests
+{
+    [TestClass]
+    public class ResponseTests
+    {
+        [TestMethod]
+        public void TestResponseProperties()
+        {
+            var response = new Response();
+            Assert.IsTrue(response.IsSuccessful);
+            response.Error = new ApiError(400);
+            Assert.IsFalse(response.IsSuccessful);
+
+            var responseT = new Response<string>();
+            Assert.IsTrue(responseT.IsSuccessful);
+            responseT.Error = new ApiError(400);
+            Assert.IsFalse(responseT.IsSuccessful);
+        }
+    }
+}
