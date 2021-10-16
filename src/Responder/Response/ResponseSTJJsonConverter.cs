e6090ce43d369ca1ba6c99f6a3c33278c96f129f
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2021-10-16 01:46:20 -0400

    #64: Added converter for System.Text.Json that is version independent

---
+++ src/Responder/Response/ResponseSTJJsonConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Phlank.Responder
+{
+    internal class ResponseSTJJsonConverter<T> : JsonConverter<Response<T>> where T : class
+    {
+        public override Response<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var serializableResponse = JsonSerializer.Deserialize<SerializableResponse<T>>(ref reader, options);
+            return serializableResponse.ToResponse();
+        }
+
+        public override void Write(Utf8JsonWriter writer, Response<T> value, JsonSerializerOptions options)
+        {
+            var serializableResponse = value.ToSerializableResponse();
+            JsonSerializer.Serialize(writer, serializableResponse, options);
+        }
+    }
+}
